a1116641a2314afca3b2a64e1423656e7e73c2ca
Jones-SnowCone <joelwhyte02@gmail.com>
2021-12-21 21:40:50 -0500

    forgot a const variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const server = http.createServer(app);
 
 app.use("/static", express.static(__dirname + "/static"));
