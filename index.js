3179965c979faa7a6ba73c4148455f7360903678
Jones-SnowCone <joelwhyte02@gmail.com>
2021-12-21 21:56:05 -0500

    Im trying here

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const http = require("http");
 const path = require("path");
 const server = http.createServer(app);
 
-app.use("/static", express.static(__dirname + "/static"));
+app.set("port", 3000);
+
+app.use("/static", express.static(path.join(__dirname, "static", "css")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
