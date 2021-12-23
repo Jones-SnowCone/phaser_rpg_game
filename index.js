716e070373ae7eee958141745ff2104b5260dcf7
Jones-SnowCone <joelwhyte02@gmail.com>
2021-12-22 22:28:47 -0500

    And we are live

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const app = express();
 const http = require("http");
 const path = require("path");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static("./"));
 
 app.get("/", (req, res) => {
     res.sendFile((__dirname + "/index.html"));
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected to the server!");
+    socket.on("disconnect", () => {
+        console.log("The user disconnected!");
+    });
+});
+
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+    });
 });
 
 server.listen(3000, () => {
