0b087683aa21ed9d488fed8ad87be7e9a9056019
Jones-SnowCone <78813982+Jones-SnowCone@users.noreply.github.com>
2022-02-10 12:29:16 -0500

    removed path module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
